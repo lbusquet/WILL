476bb5ec38de4758f1d72aefca8bbd1ba63a37cf
Lautaro Busquet <113254988+lbusquet@users.noreply.github.com>
2022-11-02 19:26:47 -0300

    Update 02.test.js

--- tests/02.test.js
+++ tests/02.test.js
@@ -2,7 +2,7 @@ const stringMasLarga = require('../02.js');
 
 describe('stringMasLarga', function() {
     it('should return "guten tag"', function() {
-      expect(stringMasLarga(['hi', 'hello', 'nihao', 'gutentag'])).toBe('gutentag');
+      expect(stringMasLarga(['hi', 'hello', 'ni hao', 'guten tag'])).toBe('guten tag');
     });
     it('should return "JavaScript"', function() {
       expect(stringMasLarga(['JavaScript', 'HTML', 'CSS'])).toBe('JavaScript');
